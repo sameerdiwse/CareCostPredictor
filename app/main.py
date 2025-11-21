3209d551dabb98eac7c6c8266c85b75c3d65fc8f
sameerdiwse <sameerdiwse9090@gmail.com>
2025-11-21 22:27:44 +0530

    pushing code changes

---
+++ app/main.py
@@ -0,0 +1,8 @@
+# codebasics ML course: codebasics.io, all rights reserverd
+
+import streamlit as st
+
+# Define the page layout
+st.title('Health Insurance Cost Predictor')
+
+st.button('predict the price')
