095084eb69bc170eaab129d00b3a191b3199da60
sameerdiwse <sameerdiwse9090@gmail.com>
2025-11-21 22:32:34 +0530

    pushing code

--- app/main.py
+++ app/main.py
@@ -28,5 +28,9 @@ row4 = st.columns(3)
 
 with row1[0]:
     age = st.number_input('Age', min_value=18, step=1, max_value=100)
+with row1[1]:
+    number_of_dependants = st.number_input('Number of Dependants', min_value=0, step=1, max_value=20)
+with row1[2]:
+    income_lakhs = st.number_input('Income in Lakhs', step=1, min_value=0, max_value=200)
 
 st.button('predict the price')
